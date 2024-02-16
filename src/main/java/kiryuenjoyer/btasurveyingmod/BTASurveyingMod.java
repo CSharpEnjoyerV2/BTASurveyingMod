5f757155fcd673158486bf8e20a4e881c8a7a53d
CSharpEnjoyer <JL03P@outlook.com>
2024-02-16 22:18:56 +0200

    Block Model Now Drops Correct Item

--- src/main/java/kiryuenjoyer/btasurveyingmod/BTASurveyingMod.java
+++ src/main/java/kiryuenjoyer/btasurveyingmod/BTASurveyingMod.java
@@ -1,12 +1,16 @@
 package kiryuenjoyer.btasurveyingmod;
 
+import kiryuenjoyer.btasurveyingmod.block.blockTheodolite;
+import kiryuenjoyer.btasurveyingmod.item.ItemTheodolite;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.client.sound.block.BlockSound;
+import net.minecraft.client.sound.block.BlockSounds;
 import net.minecraft.core.block.Block;
 import net.minecraft.core.block.material.Material;
 import net.minecraft.core.block.tag.BlockTags;
 import net.minecraft.core.item.Item;
 import net.minecraft.core.item.ItemPlaceable;
+import net.minecraft.core.item.block.ItemBlock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import turniplabs.halplibe.helper.BlockBuilder;
@@ -22,7 +26,7 @@ import java.util.Properties;
 
 
 public class BTASurveyingMod implements ModInitializer, GameStartEntrypoint, RecipeEntrypoint {
-    public static final String MOD_ID = "btasurveyingmod";
+	public static final String MOD_ID = "btasurveyingmod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	public static ConfigHandler config;
 	private static int itemID;
@@ -32,14 +36,20 @@ public class BTASurveyingMod implements ModInitializer, GameStartEntrypoint, Rec
 		config = new ConfigHandler(MOD_ID, properties);
 		itemID = config.getInt("itemID");
 	}
+
 	//Blocks
 	public static final Block blockTheodolite = new BlockBuilder(MOD_ID)
 		.setBlockModel(new BlockModelDragonFly(ModelHelper.getOrCreateBlockModel(MOD_ID, "block/blockTheodolite.json")))
-		.build(new BlockModel("blockTheodolite", 1000, Material.metal, ModelHelper.getOrCreateBlockModel(MOD_ID, "block/blockTheodolite.json")));
+		.setHardness(2.0F)
+		.setBlockSound(BlockSounds.STONE)
+		.setTags(BlockTags.MINEABLE_BY_PICKAXE)
+		.build(new blockTheodolite("blockTheodolite", 1000, Material.metal, ModelHelper.getOrCreateBlockModel(MOD_ID, "block/blockTheodolite.json")));
 
 	//Items
-	public static Item ItemTheodolite = ItemHelper.createItem(BTASurveyingMod.MOD_ID,
+	public static Item itemTheodolite = ItemHelper.createItem(BTASurveyingMod.MOD_ID,
 		new ItemPlaceable("theodolite", itemID++, blockTheodolite), "btasurveyingmodtheodolite.png");
+
+
 	@Override
     public void onInitialize() {
         LOGGER.info("btasurveyingmod initialized.");
--- src/main/java/kiryuenjoyer/btasurveyingmod/block/blockTheodolite.java
+++ src/main/java/kiryuenjoyer/btasurveyingmod/block/blockTheodolite.java
@@ -1,16 +1,26 @@
 package kiryuenjoyer.btasurveyingmod.block;
 
+import kiryuenjoyer.btasurveyingmod.BTASurveyingMod;
 import net.minecraft.core.block.Block;
+import net.minecraft.core.block.BlockTransparent;
+import net.minecraft.core.block.entity.TileEntity;
 import net.minecraft.core.block.material.Material;
+import net.minecraft.core.enums.EnumDropCause;
+import net.minecraft.core.item.ItemStack;
+import net.minecraft.core.world.World;
 import useless.dragonfly.model.block.processed.BlockModel;
 
-public class blockTheodolite extends Block {
+public class blockTheodolite extends BlockTransparent {
 	public useless.dragonfly.model.block.processed.BlockModel model;
-	public blockTheodolite(String key, int id, useless.dragonfly.model.block.processed.BlockModel model){
-		super(key,id, Material.metal);
-		this.setBlockBounds(1, 2, 1, 1, 2, 1);
+	public blockTheodolite(String key, int id, Material material, useless.dragonfly.model.block.processed.BlockModel model){
+		super(key,id, material, true);
 		this.model = model;
 	}
+	@Override
+	public ItemStack[] getBreakResult(World world, EnumDropCause dropCause, int x, int y, int z, int meta, TileEntity tileEntity) {
+		return new ItemStack[]{new ItemStack(BTASurveyingMod.itemTheodolite)};
+	}
+
 	@Override
 	public boolean renderAsNormalBlock(){
 		return false;
