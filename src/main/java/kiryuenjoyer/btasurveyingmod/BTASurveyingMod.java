c9a206a3c772f1bd997e0ced5e9dc0884159e46b
CSharpEnjoyer <JL03P@outlook.com>
2024-02-16 19:42:48 +0200

    First Item Implemented

--- src/main/java/kiryuenjoyer/btasurveyingmod/BTASurveyingMod.java
+++ src/main/java/kiryuenjoyer/btasurveyingmod/BTASurveyingMod.java
@@ -1,16 +1,33 @@
 package kiryuenjoyer.btasurveyingmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.core.item.Item;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import turniplabs.halplibe.helper.ItemHelper;
+import turniplabs.halplibe.util.ConfigHandler;
 import turniplabs.halplibe.util.GameStartEntrypoint;
 import turniplabs.halplibe.util.RecipeEntrypoint;
+
+import java.util.Properties;
 
 
 public class BTASurveyingMod implements ModInitializer, GameStartEntrypoint, RecipeEntrypoint {
     public static final String MOD_ID = "btasurveyingmod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-    @Override
+	public static ConfigHandler config;
+	private static int itemID;
+	static {
+		Properties properties = new Properties();
+		properties.put("itemID", "18000");
+		config = new ConfigHandler(MOD_ID, properties);
+		itemID = config.getInt("itemID");
+	}
+
+	//Items
+	public static Item ItemTheodolite = ItemHelper.createItem(BTASurveyingMod.MOD_ID,
+		new Item("theodolite", itemID++), "btasurveyingmodtheodolite.png");
+	@Override
     public void onInitialize() {
         LOGGER.info("btasurveyingmod initialized.");
     }
---
+++ src/main/java/kiryuenjoyer/btasurveyingmod/item/ItemTheodolite.java
@@ -0,0 +1,10 @@
+package kiryuenjoyer.btasurveyingmod.item;
+
+import net.minecraft.core.item.Item;
+
+public class ItemTheodolite extends Item {
+	public ItemTheodolite(String name, int id) {
+		super(name, id);
+		this.maxStackSize = 64;
+	}
+}
